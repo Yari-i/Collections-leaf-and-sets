64bd7b6bf9278a53556dab1f6f4a18a70e4a8b17
Yari <tipo47181@gmail.com>
2024-10-12 16:18:56 +0500

    start

---
+++ src/main/java/pro/sky/Listofemployeesdemo/ListOfEmployeesDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Listofemployeesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfEmployeesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfEmployeesDemoApplication.class, args);
+	}
+
+}
